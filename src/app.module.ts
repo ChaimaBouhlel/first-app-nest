9a06a2ce03aaec70a8cb83cbefced79b8330cc50
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2023-02-25 19:32:11 +0100

    add module Premier with a controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { PremierModule } from './premier/premier.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, PremierModule],
   controllers: [AppController],
   providers: [AppService],
 })
