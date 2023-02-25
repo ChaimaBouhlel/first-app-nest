9374a87c3cdba531b2a4af707b12a78d6459985d
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2023-02-25 19:20:53 +0100

    explore different ways of Post requests

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { Todo } from './entities/todo.entity';
 
 @Controller('todo')
 export class TodoController {
+  constructor() {
+    this.todos = [];
+  }
+  todos: Todo[];
   @Get()
   getTodos() {
     console.log('get todo list');
-    return 'list To do';
+    return this.todos;
   }
 
   @Get('getV2')
@@ -17,10 +22,34 @@ export class TodoController {
       content: 'todos list',
     });
   }
-
   @Post()
-  addTodos(@Req() request: Request, @Res() response: Response) {
-    console.log('get todo list');
-    return 'list To do';
+  addTodo(@Body() newTodo) {
+    console.log('add todo list');
+    console.log(newTodo);
+    return 'add new todo to the list';
+  }
+
+  @Post('postV2')
+  addTodoV2(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('description') description: string,
+  ) {
+    console.log('add todo list');
+    console.log(id, name, description);
+    return 'add new todo to the list';
+  }
+
+  @Post('postV3')
+  addTodoV3(@Body() newTodo: Todo) {
+    console.log('add todo list');
+    if (this.todos.length) {
+      newTodo.id = this.todos.length + 1;
+    } else {
+      newTodo.id = 1;
+    }
+    this.todos.push(newTodo);
+    console.log(newTodo);
+    return this.todos;
   }
 }
